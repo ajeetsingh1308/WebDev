949d4a848ecdb0db10e8273a44cb3f6079a45a1c
Ajeet Singh <147801619+ajeetsingh1308@users.noreply.github.com>
2025-04-19 07:59:23 +0000

    commit

---
+++ 01_Basic/03_Convertion.js
@@ -0,0 +1,11 @@
+let Age = 30;
+let greet = "Namaste";
+let isSeniorCitizen = "yes";
+let Rank= null;
+
+let rankinNumber= Number(Rank);
+let isSeniorCitizeninBoolean = Boolean(isSeniorCitizen);
+// console.log(rankinNumber);
+// console.log(typeof(rankinNumber));
+console.log(isSeniorCitizeninBoolean);
+console.log(typeof(isSeniorCitizeninBoolean));
